9fa949b6c2fd9193af30f71fb046e0a6b4cff557
Arturo Sanchez <arturosanchez@Mac-mini-de-Arturo.local>
2022-11-28 23:27:07 -0600

    Se agrega > faltante al metodo getPopularMovies

--- domain/src/main/java/com/applications/asm/domain/repository/MoviesRepository.java
+++ domain/src/main/java/com/applications/asm/domain/repository/MoviesRepository.java
@@ -5,12 +5,10 @@ import com.applications.asm.domain.entities.MovieSummary;
 import com.applications.asm.domain.entities.Movies;
 import com.applications.asm.domain.entities.Response;
 
-import java.util.List;
-
 import io.reactivex.rxjava3.core.Single;
 
 public interface MoviesRepository {
-    Single<Response<Movies<MovieSummary>> getPopularMovies(String language, Integer page);
+    Single<Response<Movies<MovieSummary>>> getPopularMovies(String language, Integer page);
     Single<Response<Movies<MovieSummary>>> getTopRateMovies(String language, Integer page);
     Single<Response<Movies<MovieSummary>>> getFavoritesMovies(String language);
     Single<Response<MovieDetails>> getMovieDetails(String language, Integer movieId);
